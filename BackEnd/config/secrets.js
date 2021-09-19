35fa46ecb23b26c14d17fa902ff29234567b8250
= <=>
2021-09-19 09:11:06 -0700

    Deployn food app

---
+++ BackEnd/config/secrets.js
@@ -0,0 +1,7 @@
+module.exports={
+
+    DB_LINK : "mongodb+srv://admin:admin@cluster0.hzqjf.mongodb.net/food?retryWrites=true&w=majority",
+    SECRET_KEY : "kanscwenco487rfhuedjsah283rhiewfoichq23",
+    APP_KEY : "kbjrrldfllqkogkz"
+
+}
